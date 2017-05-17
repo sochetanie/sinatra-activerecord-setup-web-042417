74e213659eabeaf13de8ce3e0dd09155ee03fb18
sochetanie <allagorik2901@gmail.com>
2017-05-17 09:39:07 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+	configure :development do
+	  set :database, 'sqlite3:db/database.db'
+	  # 'set :database, 'sqlite3:db/dogs.db' if file call dogs.dd or any other db file name we use
+	end
+
 require './app'
